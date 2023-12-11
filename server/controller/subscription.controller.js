c327169475d675dd8d47671cc4561cfc1385015c
G4Goku <g4gokulsuresh@gmail.com>
2023-12-11 18:32:35 +0530

    Price mapping

--- server/controller/subscription.controller.js
+++ server/controller/subscription.controller.js
@@ -4,8 +4,10 @@ const subscriptionService = require('../service/subscription.service')
 const listPrices = async (req, res) => {
     try {
         const prices = await subscriptionService.list()
+        const reversePrice = prices.data.reverse()
+        console.log({reversePrice})
         if (!prices && prices.data.length <= 0) return res.status(400).send({ message: constants.PRICE_LIST_FAILED })
-            return res.status(200).send({ prices, message: constants.PRICE_LIST_SUCCESS })
+            return res.status(200).send({ prices: reversePrice, message: constants.PRICE_LIST_SUCCESS })
     } catch (error) {
         return res.status(500).send({ message: error.message })
     }
